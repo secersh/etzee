d2d963c1d1490e9c070a8aa11220067265bb4983
Nikola Secerovski <secersh@gmail.com>
2025-05-06 18:53:51 +0200

    fix: fixed py script

--- ecad/scripts/emf-surfer.py
+++ ecad/scripts/emf-surfer.py
@@ -5,7 +5,7 @@ board = pcbnew.GetBoard()
 
 # Parameters
 number_of_slots = 5
-number_of_tracks = 30
+number_of_tracks = 17
 outer_radius = 59
 inner_radius = 35
 track_width = 0.15
@@ -22,29 +22,41 @@ def rotate_point(x, y, cx, cy, angle_degrees):
 
     return pcbnew.VECTOR2I(pcbnew.FromMM(x_rot + cx), pcbnew.FromMM(y_rot + cy))
 
-def add_track(start_point, end_point):
+def add_track(start_point, end_point, layer):
     track = pcbnew.PCB_TRACK(board)
     track.SetStart(start_point)
     track.SetEnd(end_point)
     track.SetWidth(pcbnew.FromMM(track_width))
-    track.SetLayer(pcbnew.F_Cu)
+    track.SetLayer(layer)
     board.Add(track)
 
-def add_arc(start_point, mid_point, end_point):
+def add_arc(start_point, mid_point, end_point, layer):
     arc = pcbnew.PCB_ARC(board)
     arc.SetStart(start_point)
     arc.SetMid(mid_point)
     arc.SetEnd(end_point)
     arc.SetWidth(pcbnew.FromMM(track_width))
-    arc.SetLayer(pcbnew.F_Cu)
+    arc.SetLayer(layer)
     board.Add(arc)
 
-def draw(phase):
+def add_circle(radius):
+    circle = pcbnew.PCB_SHAPE(board)
+    circle.SetShape(pcbnew.S_CIRCLE)
+    circle.SetCenter(pcbnew.VECTOR2I(pcbnew.FromMM(origin[0]), pcbnew.FromMM(origin[1])))
+    circle.SetWidth(pcbnew.FromMM(track_width))
+    circle.SetLayer(pcbnew.F_SilkS)
+    circle.SetEnd(pcbnew.VECTOR2I(pcbnew.FromMM(origin[0] + radius), pcbnew.FromMM(origin[1])))
+    board.Add(circle)
+
+def draw(phase, layer):
     track_spacing = 2 * track_width
+    tracks_width = track_spacing * number_of_tracks
+    half_width = tracks_width / 2
     tracks_angle = 360 / number_of_slots
     ox = origin[0]
     oy = origin[1]
 
+    starting_slot_outer_points = [None] * number_of_tracks
     previous_slot_outer_points = [None] * number_of_tracks
 
     for slot in range(number_of_slots):
@@ -56,47 +68,61 @@ def draw(phase):
 
         for track_number in range(number_of_tracks):
 
-            calulated_track_spacing = track_spacing * track_number
-            track_inner_radius = inner_radius + calulated_track_spacing
-            track_outer_radius = outer_radius + track_spacing * number_of_tracks + calulated_track_spacing
-            fillet_radius = track_width * 2 * track_number
+            calculated_width = track_width * track_number
+            calculated_width_i = track_width * (number_of_tracks - track_number)
+            calculated_track_spacing = track_spacing * track_number
+            track_inner_radius = inner_radius - tracks_width + track_width + calculated_track_spacing
+            track_outer_radius = outer_radius + calculated_track_spacing
 
-            track_offset = track_spacing * track_number - (track_spacing * (number_of_tracks - 1) / 2)
+            track_offset = track_spacing * track_number - half_width
 
-            al1 = rotate_point(ox + track_offset, oy + track_inner_radius, ox, oy, left_angle)
-            bl1 = rotate_point(ox + track_offset, oy + track_inner_radius + fillet_radius, ox, oy, left_angle)
-            cl1 = rotate_point(ox + track_offset + fillet_radius, oy + track_inner_radius + fillet_radius, ox, oy, left_angle)
-            dl1 = rotate_point(ox + track_offset + fillet_radius, oy + track_inner_radius, ox, oy, left_angle)
+            left_line_str = rotate_point(ox + track_offset, oy + inner_radius - calculated_width_i, ox, oy, left_angle)
+            left_line_end = rotate_point(ox + track_offset, oy + outer_radius + calculated_width, ox, oy, left_angle)
 
-            al2 = rotate_point(ox + track_offset, oy + track_outer_radius, ox, oy, left_angle)
-            bl2 = rotate_point(ox + track_offset, oy + track_outer_radius - fillet_radius, ox, oy, left_angle)
-            cl2 = rotate_point(ox + track_offset - fillet_radius, oy + track_outer_radius - fillet_radius, ox, oy, left_angle)
-            dl2 = rotate_point(ox + track_offset - fillet_radius, oy + track_outer_radius, ox, oy, left_angle)
+            right_line_str = rotate_point(ox - track_offset, oy + inner_radius- calculated_width_i, ox, oy, right_angle)
+            right_line_end = rotate_point(ox - track_offset, oy + outer_radius + calculated_width, ox, oy, right_angle)
 
-            ar1 = rotate_point(ox - track_offset, oy + track_inner_radius, ox, oy, right_angle)
-            br1 = rotate_point(ox - track_offset, oy + track_inner_radius + fillet_radius, ox, oy, right_angle)
-            cr1 = rotate_point(ox - track_offset + fillet_radius, oy + track_inner_radius + fillet_radius, ox, oy, right_angle)
-            dr1 = rotate_point(ox - track_offset + fillet_radius, oy + track_inner_radius, ox, oy, right_angle)
+            inner_arc_str = rotate_point(ox + half_width - calculated_width_i - track_width, oy + track_inner_radius, ox, oy, left_angle)
+            inner_arc_mid = rotate_point(ox, oy + track_inner_radius, ox, oy, slot_angle)
+            inner_arc_end = rotate_point(ox - half_width + calculated_width_i + track_width, oy + track_inner_radius, ox, oy, right_angle)
 
-            ar2 = rotate_point(ox - track_offset, oy + track_outer_radius, ox, oy, right_angle)
-            br2 = rotate_point(ox - track_offset, oy + track_outer_radius - fillet_radius, ox, oy, right_angle)
-            cr2 = rotate_point(ox - track_offset - fillet_radius, oy + track_outer_radius - fillet_radius, ox, oy, right_angle)
-            dr2 = rotate_point(ox - track_offset - fillet_radius, oy + track_outer_radius, ox, oy, right_angle)
+            outer_arc_str = rotate_point(ox - half_width + calculated_width, oy + track_outer_radius, ox, oy, left_angle)
+            outer_arc_mid = rotate_point(ox, oy + track_outer_radius, ox, oy, slot_angle)
+            outer_arc_end = rotate_point(ox + half_width - calculated_width, oy + track_outer_radius, ox, oy, right_angle)
 
-            mi = rotate_point(ox + track_offset, oy + track_inner_radius, ox, oy, slot_angle)
-            mo = rotate_point(ox - track_offset, oy + track_outer_radius, ox, oy, slot_angle)
-
-            add_arc(al1, mi, ar1)
+            add_arc(inner_arc_str, inner_arc_mid, inner_arc_end, layer)
 
             if previous_slot_outer_points[track_number] is not None:
-                add_arc(previous_slot_outer_points[track_number], mo, al2)
+                add_arc(previous_slot_outer_points[track_number], outer_arc_mid, outer_arc_str, layer)
 
-            previous_slot_outer_points[track_number] = ar2
+            previous_slot_outer_points[track_number] = outer_arc_end
 
-            add_track(al1, al2)
-            add_track(ar1, ar2)
+            if slot == 0:
+                starting_slot_outer_points[track_number] = outer_arc_str
 
-draw(0)
-# draw(24)
-# draw(48)
+            add_track(left_line_str, left_line_end, layer)
+            add_track(right_line_str, right_line_end, layer)
+
+            add_track(left_line_end, outer_arc_str, layer)
+            add_track(right_line_end, outer_arc_end, layer)
+
+            add_track(left_line_str, inner_arc_str, layer)
+            add_track(right_line_str, inner_arc_end, layer)
+
+    for track_number in range(number_of_tracks):
+
+        track_outer_radius = outer_radius + calculated_track_spacing
+
+        outer_arc_mid = rotate_point(ox, oy + track_outer_radius, ox, oy, slot_angle)
+
+        if track_number + 1 < number_of_tracks:
+            add_arc(starting_slot_outer_points[track_number], outer_arc_mid, previous_slot_outer_points[track_number + 1], layer)
+
+add_circle(inner_radius)
+add_circle(outer_radius)
+
+draw(0, pcbnew.F_Cu)
+draw(24, pcbnew.In1_Cu)
+draw(48, pcbnew.In2_Cu)
+
 pcbnew.Refresh()
